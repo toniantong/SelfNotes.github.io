54f8d3810b70bdc08ffecac02a7684f2aa7e1693
toniantong <72894635+toniantong@users.noreply.github.com>
2022-10-15 19:57:26 +0800

    v2

---
+++ advenitio-master/public/index.js
@@ -0,0 +1,5 @@
+let n = '<br />'
+document.write(`<div spellcheck="false" contenteditable="true">`);// style="padding:5px; border:1px solid black"
+document.write(`${(4 > 2) && (10 < 15)} = ${(4 > 2)} && ${(10 < 15)} = (4 > 2) && (10 < 15)`+n);
+document.write(1<2+`1<2`+n);
+document.write(`</div>`);
