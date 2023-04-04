071930eca97a3798511fc784478303aab963939d
toniantong <72894635+toniantong@users.noreply.github.com>
2023-04-05 05:45:22 +0800

    Create ONEZTT.js

---
+++ advenitio-master/public/ONEZTT.js
@@ -0,0 +1,6 @@
+let n = '<br />'
+document.write(`<div spellcheck="false" contenteditable="true">`);// style="padding:5px; border:1px solid black"
+for (let i = 0; i < 36; i++) {
+document.write(''+n);
+}
+document.write(`</div>`);
