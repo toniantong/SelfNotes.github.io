39b630276c9209e80864914203fc410a41fa197a
toniantong <72894635+toniantong@users.noreply.github.com>
2026-05-17 12:52:46 +0800

    Update public.js

--- advenitio-master/public/public.js
+++ advenitio-master/public/public.js
@@ -2,5 +2,7 @@ document.write(`
 <p class="tlike"><a href="http://www.google.com" target="_blank" rel="noopener">Google</a>
     <a href="index.html">index</a>
     <a href="ONEZTT.html" target="_blank">ONEZTT</a>
+    <br>
+    <a href=On-Test.html">On-Test</a>
 </p>
 `);
